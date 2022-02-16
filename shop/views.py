5c467afb4b6bcca45825b609b67b50961493efe1
Preeti narware <preetinarware97@gmail.com>
2022-02-16 16:48:37 +0530

    cllbck url chnge

--- shop/views.py
+++ shop/views.py
@@ -242,7 +242,7 @@ def order(request):
                 'INDUSTRY_TYPE_ID': 'Retail',
                 'WEBSITE': 'WEBSTAGING',
                 'CHANNEL_ID': 'WEB',
-                'CALLBACK_URL':'http://127.0.0.1:8000/handlerequest/', }
+                'CALLBACK_URL':'https://cyberacdamy.herokuapp.com/handlerequest/', }
                 
                 param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
                 c.delete()
@@ -279,7 +279,7 @@ def order(request):
                     'INDUSTRY_TYPE_ID': 'Retail',
                     'WEBSITE': 'WEBSTAGING',
                     'CHANNEL_ID': 'WEB',
-                    'CALLBACK_URL':'http://127.0.0.1:8000/handlerequest/', 
+                    'CALLBACK_URL':'https://cyberacdamy.herokuapp.com/handlerequest/', 
                       }  
                 param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
                
