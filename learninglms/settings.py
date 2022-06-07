0b2ed8c8c0fc194b3b828830a98ae6eaa159b02d
unknown <karunabunkar637@gmail.com>
2022-06-07 14:27:02 -0400

    fix

--- learninglms/settings.py
+++ learninglms/settings.py
@@ -158,8 +158,8 @@ USE_TZ = True
 # STATIC_ROOT=BASE_DIR / 'static'
 
 
-# STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
